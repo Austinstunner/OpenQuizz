cbe218980b6e98e927c45d467b56f25f03b08bc9
Austinstunner <71695467+Austinstunner@users.noreply.github.com>
2021-01-18 18:17:36 +0100

    Add files via upload

---
+++ OpenQuizz/OpenQuizz/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Dev on 18/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
